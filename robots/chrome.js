497677d6998c53fab5f05d7d03133798d9faeae3
WiltencirDG <wiltencir@gmail.com>
2021-07-27 18:02:30 -0300

    Fixed login on CEI

--- robots/chrome.js
+++ robots/chrome.js
@@ -1,10 +1,4 @@
-const randomUseragent = require('random-useragent')
-// const puppeteer = require('puppeteer')
-const puppeteer = require('puppeteer-extra')
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
-
-const USER_AGENT = '5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36';
+const puppeteer = require('puppeteer')
 
 async function robot(ceiCredentials, pBroker) {
 
@@ -26,7 +20,7 @@ async function robot(ceiCredentials, pBroker) {
                 '--no-sandbox',
                 '--disable-setuid-sandbox'
             ], timeout: 60000
-            , headless: false
+            , headless: true
             // , defaultViewport: true
         })
         return chrome
@@ -34,10 +28,6 @@ async function robot(ceiCredentials, pBroker) {
 
     async function openNewPage(chrome) {
         const page = await chrome.newPage({ timeout: 60000 })
-        const userAgent = randomUseragent.getRandom(function (ua) {
-            return ua.browserName === 'Chrome';
-        });
-        const UA = userAgent || USER_AGENT
 
         await page.setViewport({
             width: 1920 + Math.floor(Math.random() * 100),
@@ -48,66 +38,11 @@ async function robot(ceiCredentials, pBroker) {
             isMobile: false,
         });
 
-        await page.setUserAgent(UA);
-        await page.setJavaScriptEnabled(true);
-        await page.setDefaultNavigationTimeout(0);
-
-        await page.setRequestInterception(true);
-        page.on('request', (req) => {
-            if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
-                req.abort();
-            } else {
-                req.continue();
-            }
-        });
-
-        await page.evaluateOnNewDocument(() => {
-            // Pass webdriver check
-            Object.defineProperty(navigator, 'webdriver', {
-                get: () => false,
-            });
-        });
-
-        await page.evaluateOnNewDocument(() => {
-            // Pass chrome check
-            window.chrome = {
-                runtime: {},
-                // etc.
-            };
-        });
-
-        await page.evaluateOnNewDocument(() => {
-            //Pass notifications check
-            const originalQuery = window.navigator.permissions.query;
-            return window.navigator.permissions.query = (parameters) => (
-                parameters.name === 'notifications' ?
-                    Promise.resolve({ state: Notification.permission }) :
-                    originalQuery(parameters)
-            );
-        });
-
-        await page.evaluateOnNewDocument(() => {
-            // Overwrite the `plugins` property to use a custom getter.
-            Object.defineProperty(navigator, 'plugins', {
-                // This just needs to have `length > 0` for the current test,
-                // but we could mock the plugins too if necessary.
-                get: () => [1, 2, 3, 4, 5],
-            });
-        });
-
-        await page.evaluateOnNewDocument(() => {
-            // Overwrite the `languages` property to use a custom getter.
-            Object.defineProperty(navigator, 'languages', {
-                get: () => ['en-US', 'en'],
-            });
-        });
-
-        return page
+        return page;
     }
 
     async function navigateToCEI(page) {
-        await page.goto('https://cei.b3.com.br/CEI_Responsivo/', { waitUntil: 'networkidle2' });
-        await page.goto('https://cei.b3.com.br/CEI_Responsivo/', { waitUntil: 'networkidle2' });
+        await page.goto('https://ceiapp.b3.com.br/CEI_Responsivo/', { waitUntil: 'networkidle2' });
     }
 
     async function loginToCEI(page) {
@@ -116,20 +51,6 @@ async function robot(ceiCredentials, pBroker) {
         await page.type('#ctl00_ContentPlaceHolder1_txtSenha', ceiCredentials.pass);
 
         console.time('Took')
-
-        // await page.waitForSelector('#ctl00_ContentPlaceHolder1_dvCaptcha div div iframe')
-
-        // await page.waitFor(2000)
-
-        // const elementHandle = await page.$('#ctl00_ContentPlaceHolder1_dvCaptcha>div>div>iframe',)
-        // // const elementHandle = await page.$('#ctl00_ContentPlaceHolder1_dvCaptcha div div iframe',)
-        // const frame = await elementHandle.contentFrame()
-
-        // // await frame.click('#recaptcha-anchor')
-        // await frame.click('#recaptcha-anchor-label > span')
-
-        // const solver = new CaptchaSolver('reCAPTCHA')
-        // const codes = await solver.solve()
 
         await page.waitFor(500)
         await page.click('#ctl00_ContentPlaceHolder1_btnLogar');
@@ -148,7 +69,7 @@ async function robot(ceiCredentials, pBroker) {
     }
 
     async function navigateToAssets(page) {
-        await page.goto('https://cei.b3.com.br/CEI_Responsivo/negociacao-de-ativos.aspx', { timeout: 60000 })
+        await page.goto('https://ceiapp.b3.com.br/CEI_Responsivo/negociacao-de-ativos.aspx', { timeout: 60000 })
     }
 
     async function getAllBrokers(page, pBroker) {
@@ -180,7 +101,7 @@ async function robot(ceiCredentials, pBroker) {
             await page.select('#ctl00_ContentPlaceHolder1_ddlAgentes', broker.value)
             await page.click('#ctl00_ContentPlaceHolder1_btnConsultar')
             //await page.waitForNavigation()
-            await page.waitFor(2500)
+            await page.waitFor(1000)
 
             let table = []
 
